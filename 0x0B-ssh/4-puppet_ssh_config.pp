902eea01d2bdb82462da33bee9b93cf7146893ef
mauricioolarte <reneolarte@yahoo.com>
2020-08-07 03:20:47 +0000

    4-puppet_ssh_config.pp

--- 0x0B-ssh/4-puppet_ssh_config.pp
+++ 0x0B-ssh/4-puppet_ssh_config.pp
@@ -1,13 +1,13 @@
 #this file add lines
 file_line {'turn of password auth':
   ensure => present,
-  path   => '~/.ssh/config',
+  path   => '/etc/ssh/ssh_config',
   line   => 'PasswordAuthentication no',
   match  => '^PasswordAuthentication',
   }
 
 file_line {'turn of password auth':
   ensure => present,
-  path   => '~/.ssh/config',
+  path   => '/etc/ssh/ssh_config',
   line   => 'IdentityFile ~/.ssh/holberton',
   }
