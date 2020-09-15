714d39b5deab0249770bb8222e7bd4db8112b161
mauricioolarte <reneolarte@yahoo.com>
2020-09-15 02:24:45 +0000

    0-gather_data_from_an_API.py

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+import requests
+import sys
+
+name_employe = ''
+employees = requests.get('https://jsonplaceholder.typicode.com/users')
+for employ in employees.json():
+    if employ.get('id') == int(sys.argv[1]):
+        name_employe = employ.get('name')
+uri_to_req = ('https://jsonplaceholder.typicode.com/todos?userId=' +
+              str(sys.argv[1]))
+req = requests.get(uri_to_req)
+count = 0
+for key in req.json():
+    if key.get('completed') is True:
+        count += 1
+print('Employee {} is done with tasks({}/{}):'.format(
+      name_employe, count, len(req.json())))
+for key in req.json():
+    if key.get('completed') is True:
+        print('\t {}'.format(key.get('title')))
