24177ccde0a695a0f00649c5343c9cd02b18b376
mauricioolarte <reneolarte@yahoo.com>
2020-09-15 04:45:20 +0000

    2-export_to_JSON.py

---
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+""" this module make a request to https://jsonplaceholder.typicode.com/users'
+    and print todo list
+"""
+
+import requests
+import csv
+import sys
+
+if __name__ == "__main__":
+    name_employe = ''
+    employees = requests.get('http://jsonplaceholder.typicode.com/users')
+    for employ in employees.json():
+        if employ.get('id') == int(sys.argv[1]):
+            name_employe = employ.get('username')
+    uri_to_req = ('http://jsonplaceholder.typicode.com/todos?userId=' +
+                  str(sys.argv[1]))
+    req = requests.get(uri_to_req)
+    with open('USER_ID.csv', 'w') as f:
+        writer = csv.writer(f, quoting=csv.QUOTE_ALL)
+        for task in req.json():
+            USER_ID = int(sys.argv[1])
+            USERNAME = name_employe
+            TASK_COMPLETED_STATUS = str(task.get('completed'))
+            TASK_TITLE = task.get('title')
+            csv_row = [USER_ID, USERNAME, TASK_COMPLETED_STATUS, TASK_TITLE]
+            writer.writerow(csv_row)
