4ac4b03c552f6fa38f85526cb571cd510f3e2a3d
mauricioolarte <reneolarte@yahoo.com>
2020-09-15 04:34:51 +0000

    1-export_to_CSV.py

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -16,8 +16,13 @@ if __name__ == "__main__":
     uri_to_req = ('http://jsonplaceholder.typicode.com/todos?userId=' +
                   str(sys.argv[1]))
     req = requests.get(uri_to_req)
-    csv_file = csv.writer(open('USER_ID.csv', 'w'))
-    for task in req.json():
-        csv_row = [str(sys.argv[1]), name_employe, task.get('completed'),
-                task.get('title')]
-        csv_file.writerow(csv_row)
+    csv.register_dialect('unixpwd', delimiter=':', quoting=csv.QUOTE_NONE)
+    with open('USER_ID.csv', 'w', newline='') as f:
+        writer = csv.writer(f, quoting=csv.QUOTE_ALL)
+        for task in req.json():
+            USER_ID = str(sys.argv[1])
+            USERNAME = name_employe
+            TASK_COMPLETED_STATUS = str(task.get('completed'))
+            TASK_TITLE = task.get('title')
+            csv_row = [USER_ID, USERNAME, TASK_COMPLETED_STATUS, TASK_TITLE]
+            writer.writerow(csv_row)
