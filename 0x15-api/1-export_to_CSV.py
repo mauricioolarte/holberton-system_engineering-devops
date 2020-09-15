d2992dc50ceb4749b92ec7015043de91aca72778
mauricioolarte <reneolarte@yahoo.com>
2020-09-15 05:30:16 +0000

    1-export_to_CSV.py

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -12,7 +12,8 @@ if __name__ == "__main__":
     employees = requests.get('http://jsonplaceholder.typicode.com/users')
     for employ in employees.json():
         if employ.get('id') == int(sys.argv[1]):
-            name_employe = employ.get('username')
+            username_employe = employ.get('username')
+            name_employe = employ.get('name')
     uri_to_req = ('http://jsonplaceholder.typicode.com/todos?userId=' +
                   str(sys.argv[1]))
     req = requests.get(uri_to_req)
@@ -20,8 +21,18 @@ if __name__ == "__main__":
         writer = csv.writer(f, quoting=csv.QUOTE_ALL)
         for task in req.json():
             USER_ID = task.get('userId')
-            USERNAME = name_employe
+            USERNAME = username_employe
             TASK_COMPLETED_STATUS = str(task.get('completed'))
             TASK_TITLE = task.get('title')
             csv_row = [USER_ID, USERNAME, TASK_COMPLETED_STATUS, TASK_TITLE]
             writer.writerow(csv_row)
+
+    count = 0
+    for key in req.json():
+        if key.get('completed') is True:
+            count += 1
+    print('Employee {} is done with tasks({}/{}):'.format(
+        name_employe, count, len(req.json())))
+    for key in req.json():
+        if key.get('completed') is True:
+            print('\t {}'.format(key.get('title')))
