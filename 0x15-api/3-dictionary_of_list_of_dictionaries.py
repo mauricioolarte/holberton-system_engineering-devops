32591b363098180222caf0f2321dc56cfa299aa8
mauricioolarte <reneolarte@yahoo.com>
2020-09-15 06:21:33 +0000

    3-dictionary_of_list_of_dictionaries.py

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -3,25 +3,32 @@
     and print todo list
 """
 
+import json
 import requests
-import csv
 import sys
 
 if __name__ == "__main__":
-    name_employe = ''
     employees = requests.get('http://jsonplaceholder.typicode.com/users')
+    req = requests.get('http://jsonplaceholder.typicode.com/todos')
+    list = []
+    resp = {}
     for employ in employees.json():
-        if employ.get('id') == int(sys.argv[1]):
-            name_employe = employ.get('username')
-    uri_to_req = ('http://jsonplaceholder.typicode.com/todos?userId=' +
-                  str(sys.argv[1]))
-    req = requests.get(uri_to_req)
-    with open('USER_ID.csv', 'w') as f:
-        writer = csv.writer(f, quoting=csv.QUOTE_ALL)
-        for task in req.json():
-            USER_ID = int(sys.argv[1])
-            USERNAME = name_employe
-            TASK_COMPLETED_STATUS = str(task.get('completed'))
-            TASK_TITLE = task.get('title')
-            csv_row = [USER_ID, USERNAME, TASK_COMPLETED_STATUS, TASK_TITLE]
-            writer.writerow(csv_row)
+        resp[employ.get('id')] = []
+    for item in req.json():
+        # print(item)
+
+        name_employe = ''
+
+        for employ in employees.json():
+            if employ.get('id') == item.get('userId'):
+                username_employe = employ.get('username')
+
+        task = {}
+        task['username'] = username_employe
+        task['completed'] = str(item.get('completed'))
+        task['task'] = item.get('title')
+
+        resp[item.get('userId')].append(task)
+
+    with open('USER_ID.json', 'w') as file:
+        json.dump(resp, file)
