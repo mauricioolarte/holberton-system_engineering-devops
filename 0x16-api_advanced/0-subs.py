b3a59491b421c7d7175d6c9f1029490f7998be7c
mauricioolarte <reneolarte@yahoo.com>
2020-09-17 21:28:33 -0500

    0-subs.py

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+""" make request to redit api """
+import requests
+
+
+def number_of_subscribers(subreddit):
+    """ make request to subredit and return numbers of subscribers"""
+    headers = {'User-Agent': 'Mauricio'}
+    url = 'http://www.reddit.com/r/' + subreddit + '/about/.json'
+    r = requests.get(url, headers=headers)
+    answer = r.json().get('data').get('subscribers')
+    return(answer)
