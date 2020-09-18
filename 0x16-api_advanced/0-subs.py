c0f112d2006138392799458f5f76c4154540ebca
mauricioolarte <reneolarte@yahoo.com>
2020-09-17 21:34:29 -0500

    0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -8,8 +8,8 @@ def number_of_subscribers(subreddit):
     headers = {'User-Agent': 'Mauricio'}
     url = 'http://www.reddit.com/r/' + subreddit + '/about/.json'
     r = requests.get(url, headers=headers)
-    answer = r.json().get('data').get('subscribers')
-    if answer is None:
-        return(0)
-    else:
+    if r.status_code == 200:
+        answer = r.json().get('data').get('subscribers')
         return(answer)
+    else:
+        return(0)
